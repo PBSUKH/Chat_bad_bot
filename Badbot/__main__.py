669cfd7b1702240da3050a42650b026fc67b733c
SUKH ❤️ <158456267+PBSUKH@users.noreply.github.com>
2024-02-21 19:06:13 +0530

    Add files via upload

---
+++ Badbot/__main__.py
@@ -0,0 +1,22 @@
+import asyncio
+import importlib
+from pyrogram import idle
+from Badbot import app
+from Badbot.modules import ALL_MODULES
+
+LOGGER_ID = -1002056907061
+
+loop = asyncio.get_event_loop()
+
+async def Bad_boot():
+    for all_module in ALL_MODULES:
+        importlib.import_module("Badbot.modules." + all_module)
+    print("𝖻𝗈𝗍 𝗌𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝗌𝗍𝖺𝗋𝗍")
+    await app.send_message(LOGGER_ID, "**𝖨 𝖺𝗆 𝖺𝗅𝗂𝗏𝖾 𝖡𝖺𝖻𝗒 𝖸𝗈𝗎𝗋 𝖡𝗈𝗍 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝖣𝖾𝗉𝗅𝗈𝗒 \n Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ  [🕊️⃝‌ٖٖٖٖ ‌ٖٖٖٖٖ𝐁α∂ ❤️ᥫ᭡፝֟፝֟](https://t.me/II_BAD_BBY_II)**")
+    await idle()
+    print("𝖻𝖺𝗁𝖺𝗇𝖼𝗁𝗈𝖽 𝖯𝗂𝗋𝗈 𝖢𝗈𝖽𝖾𝗋 𝗄𝗋𝗅𝗈 𝖾𝖽𝗂𝗍 𝖺𝖺 𝗀𝗒𝖺 𝗇 𝖾𝗋𝗋𝗈𝗋 𝖺𝖺𝖻 𝗃𝖺𝗄𝖾 𝗀𝖺𝗇𝖽 𝗆𝖺𝗋𝗐𝖺𝗈 𝗂𝗌𝗌𝖾 @II_BAD_BBY_II ")
+  
+  
+if __name__ == "__main__":
+    loop.run_until_complete(Bad_boot())
+    
