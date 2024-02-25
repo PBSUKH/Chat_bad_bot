2585972974b283cbf24830eb27759dcfed4aa453
SUKH ❤️ <158456267+PBSUKH@users.noreply.github.com>
2024-02-25 11:39:15 +0530

    Update bass.py

--- Badbot/modules/bass.py
+++ Badbot/modules/bass.py
@@ -45,4 +45,48 @@ def apply_bass_boost(audio_path):
     return boosted_audio_path
 
 
+@app.on_message(filters.command("loudly"))
+async def download_and_enhance_audio(client, message):
+    try:
+        reply_message = message.reply_to_message
+
+        if reply_message.audio:
+            msg = await message.reply("processing")
+            audio = await reply_message.download()
+            audio_segment = pydub.AudioSegment.from_file(audio)
+            await msg.edit("now adding loude audio and uploading...")
+        
+            louder_audio = audio_segment + 10
+            
+            louder_audio.export("chizuru.mp3", format="mp3")
+            await msg.delete()
+            await message.reply_audio("chizuru.mp3")
+        else:
+            await message.reply("The replied message is not an audio.")
+    except Exception as e:
+        await message.reply(f"An error occurred: {str(e)}")
+
+
+
+
+
+@app.on_message(filters.command("mono"))
+async def split_stereo_and_send_audio(client, message):
+    try:
+        reply_message = message.reply_to_message
+
+        if reply_message.audio:
+            msg = await message.reply("processing")
+            a = pydub.AudioSegment.from_file(await reply_message.download())
+            b = a.split_to_mono()
+            mono_audio = b[0]
+            await msg.edit("now adding mono audio and uploading...")
+            
+            mono_audio.export("chizuru.mp3", format="mp3")
+            await msg.delete()
+            await message.reply_audio("chizuru.mp3")
+        else:
+            await message.reply("The replied message is not an audio.")
+    except Exception as e:
+        await message.reply(f"An error occurred: {str(e)}")
 
