28ae4f2f79960caa6c8528dda197476763929518
SUKH ❤️ <158456267+PBSUKH@users.noreply.github.com>
2024-02-25 22:14:50 +0530

    Update start.py

--- Badbot/modules/start.py
+++ Badbot/modules/start.py
@@ -73,7 +73,7 @@ chizuru_buttons = [
                     InlineKeyboardButton("✯ ᴡʜɪsᴘᴇʀ ✯", callback_data="sukh_")
                 ],
                [
-                       InlineKeyboardButton("✯ sʜɪᴢᴜ ʙᴏᴛ ✯", callback_data="spical_")
+                       InlineKeyboardButton("✯ sʜɪᴢᴜ ʙᴏᴛ ✯", callback_data="sk")
                ],
                 [
                     InlineKeyboardButton("⟲ ʙᴀᴄᴋ ⟳", callback_data="home_"),
@@ -233,7 +233,15 @@ async def cb_handler(client, query):
         except MessageNotModified:
             pass
 
-     
+     elif query.data=="sk":        
+        reply_markup = InlineKeyboardMarkup(back_buttons)
+        try:
+            await query.edit_message_text(
+                spical_txt,
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass
        
 # ------------------------------------------------------------------------------- #
 
