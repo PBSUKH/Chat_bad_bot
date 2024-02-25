f5a215ceb9751e41bd009db203fc1021c5ae0aee
SUKH ❤️ <158456267+PBSUKH@users.noreply.github.com>
2024-02-25 14:28:05 +0530

    Update ping.py

--- Badbot/modules/ping.py
+++ Badbot/modules/ping.py
@@ -1,4 +1,4 @@
-from Badmunda import app 
+from Badbot import app 
 import time, os
 from pyrogram import filters, __version__
 from pytgcalls import __version__ as version
@@ -13,7 +13,7 @@ ping_txt = """
 ⌬ **Pytgcalls** - `{}`
 """
 
-@Chizuru.on_message(filters.command("ping"))
+@app.on_message(filters.command("ping"))
 async def ping_cmd(client, message):
     start_time = time.time()
     a = await message.reply("Pinging...")
@@ -21,7 +21,7 @@ async def ping_cmd(client, message):
     ping_time = round((end_time - start_time) * 1000)    
     await a.edit_text(ping_txt.format(ping_time,__version__, version))
 
-@Chizuru.on_message(filters.command(["cleanup"]))
+@app.on_message(filters.command("cleanup"))
 async def cleanup(_, message):
     pth = os.path.realpath(".")
     ls_dir = os.listdir(pth)
