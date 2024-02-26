256d3c409f16d1a443e46bd6d75d166ddcbc0880
SUKH ❤️ <158456267+PBSUKH@users.noreply.github.com>
2024-02-26 19:11:51 +0530

    Create inline.py

---
+++ Badbot/core/inline.py
@@ -0,0 +1,13 @@
+from pyrogram.types import InlineKeyboardButton
+
+from Badbot import OWNER
+from Badbot import MickeyBot
+
+
+
+CHATBOT_ON = [
+    [
+        InlineKeyboardButton(text="ᴇɴᴀʙʟᴇ", callback_data=f"addchat"),
+        InlineKeyboardButton(text="ᴅɪsᴀʙʟᴇ", callback_data=f"rmchat"),
+    ],
+]
