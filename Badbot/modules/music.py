4da4736c4ae273bd63e1f2646b49e3414c686c45
SUKH ❤️ <158456267+PBSUKH@users.noreply.github.com>
2024-02-26 17:36:05 +0530

    Update music.py

--- Badbot/modules/music.py
+++ Badbot/modules/music.py
@@ -23,11 +23,12 @@ DURATION_LIMIT = 300
 
 keyboard = InlineKeyboardMarkup([
         [
-                InlineKeyboardButton("•❤️⃤• ılııılılıılıııllıılııılııılıılıılıılıılı. ♡♪🦋•", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+                InlineKeyboardButton("        ..........⬬⭒💘⭒⬬......... 
+•┈┈┈••┈┈┈••••●••••┈┈┈••┈┈┈•", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
-        [InlineKeyboardButton(" ◁ ", url=f"https://t.me/ll_THE_BAD_BOT_ll"),
+        [InlineKeyboardButton(" ◁ ", url=f"https://t.me/ll_BAD_ABOUT_ll"),
          InlineKeyboardButton(" ❚❚ ", url=f"https://t.me/ll_THE_BAD_BOT_ll"),
-         InlineKeyboardButton(" ▷ ", url=f"https://t.me/ll_THE_BAD_BOT_ll")
+         InlineKeyboardButton(" ▷ ", url=f"https://t.me/ll_BAD_MUNDA_WORLD_ll")
         ],
         [
             InlineKeyboardButton(" ᴄʟᴏsᴇ ", callback_data="close_data"),
